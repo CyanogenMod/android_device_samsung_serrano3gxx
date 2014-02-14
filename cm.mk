3af55c0e77ddb2cb8934f85d62819028f5f21016
Arne Coucheron <arco68@gmail.com>
2014-02-14 02:14:42 +0100

    serrano3gxx: Change inherit order
     * Move comon_full_phone.mk to top. Otherwise default notification
       and alarm tone will be completely wrong.
    
    Change-Id: I7fcc4709807dce7ed91c8e9c85eea39f694dd445

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
-$(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serrano3gx TARGET_DEVICE=serrano3g BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.2.2/JDQ39/I9190XXUAMF7:user/release-keys" PRIVATE_BUILD_DESC="serrano3gxx-user 4.2.2 JDQ39 I9190XXUAMF7 release-keys"
 
