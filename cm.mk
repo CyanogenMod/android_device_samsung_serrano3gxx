ecc32a079df1d024a1caee5cad353ce760286bf4
Arne Coucheron <arco68@gmail.com>
2014-07-24 06:15:43 +0200

    serrano3gxx: Update fingerprint
    Change-Id: I5b0c56080d1f5bb7c8b4580515ab0643fa796df8

--- cm.mk
+++ cm.mk
@@ -3,7 +3,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serrano3gx TARGET_DEVICE=serrano3g BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.2.2/JDQ39/I9190XXUAMF7:user/release-keys" PRIVATE_BUILD_DESC="serrano3gxx-user 4.2.2 JDQ39 I9190XXUAMF7 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serrano3gx TARGET_DEVICE=serrano3g BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.4.2/KOT49H/I9190XXUCNG1:user/release-keys" PRIVATE_BUILD_DESC="serrano3gxx-user 4.4.2 KOT49H I9190XXUCNG1 release-keys"
 
 PRODUCT_DEVICE := serrano3gxx
 PRODUCT_NAME := cm_serrano3gxx
