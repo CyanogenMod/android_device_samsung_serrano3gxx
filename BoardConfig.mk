91115e8a77521d92c4f429f317da34e86a8d8406
Arne Coucheron <arco68@gmail.com>
2016-02-10 02:43:19 +0100

    Revert "init: Set device serial number via vendor init"
    This reverts commit 0ef6279c98974c4827f76a6ca03619948794b74e.
    
    Change-Id: I7fac70197bd60945d1179e4df3e11a9818e53262

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,3 @@ TARGET_KERNEL_VARIANT_CONFIG := msm8930_serrano_eur_3g_defconfig
 
 # BLN
 BOARD_HAVE_GENERIC_BLN := true
-
-# Vendor init
-TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := device/samsung/serrano3gxx/init/init_serrano3g.cpp
