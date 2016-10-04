91f9d5426d57b78b65f1b794ebeb7fe6258a928c
Arne Coucheron <arco68@gmail.com>
2016-10-04 22:25:25 +0200

    Initial N bringup
    Change-Id: I505b97ebca482daa729162f3ae814a1f95c62be9

--- cm.mk
+++ cm.mk
@@ -1,9 +1,33 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from serrano3gxx device
+$(call inherit-product, device/samsung/serrano3gxx/device.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, device/samsung/serrano3gxx/full_serrano3gxx.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serrano3gxx TARGET_DEVICE=serrano3g BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.4.2/KOT49H/I9190XXUCNG1:user/release-keys" PRIVATE_BUILD_DESC="serrano3gxx-user 4.4.2 KOT49H I9190XXUCNG1 release-keys"
-
-PRODUCT_DEVICE := serrano3gxx
 PRODUCT_NAME := cm_serrano3gxx
+PRODUCT_DEVICE := serrano3gxx
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := GT-I9190
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=serrano3gxx \
+    BUILD_FINGERPRINT="samsung/serrano3gxx/serrano3g:4.4.2/KOT49H/I9190XXUCNG1:user/release-keys" \
+    PRIVATE_BUILD_DESC="serrano3gxx-user 4.4.2 KOT49H I9190XXUCNG1 release-keys"
